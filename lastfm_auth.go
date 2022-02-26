534cea01c1126f1559537e86775fadaafa2c3bd3
pd <xprnvd@outlook.com>
2022-02-26 22:05:17 +0530

    lastfm_auth

--- lastfm_auth.go
+++ lastfm_auth.go
@@ -1,4 +1,4 @@
-package lastfm_auth
+package main
 
 import (
 	"encoding/json"
---
+++ yamaha_request.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+// Response is the struct that will be unmarshalled from the JSON response
+type Response struct {
+	ResponseCode     int      `json:"response_code"`
+	Input            string   `json:"input"`
+	PlayQueueType    string   `json:"play_queue_type"`
+	Playback         string   `json:"playback"`
+	Repeat           string   `json:"repeat"`
+	Shuffle          string   `json:"shuffle"`
+	PlayTime         int      `json:"play_time"`
+	TotalTime        int      `json:"total_time"`
+	Artist           string   `json:"artist"`
+	Album            string   `json:"album"`
+	Track            string   `json:"track"`
+	AlbumartURL      string   `json:"albumart_url"`
+	AlbumartID       int      `json:"albumart_id"`
+	UsbDevicetype    string   `json:"usb_devicetype"`
+	AutoStopped      bool     `json:"auto_stopped"`
+	Attribute        int      `json:"attribute"`
+	RepeatAvailable  []string `json:"repeat_available"`
+	ShuffleAvailable []string `json:"shuffle_available"`
+}
+
+//function to handle status request from yamaha
+func yamaha_request() {
+	resp, err := http.Get("http://192.168.0.243:80/YamahaExtendedControl/v1/netusb/getPlayInfo")
+	if err != nil {
+		fmt.Println("No response from request")
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body) // response body is []byte
+	if err != nil {
+		fmt.Println("Can not read response body")
+	}
+
+	var result Response
+	if err := json.Unmarshal(body, &result); err != nil { // Parse []byte to the go struct pointer
+		fmt.Println("Can not unmarshal JSON")
+	}
+
+	fmt.Println(result.Artist, result.Album, result.Track, result.PlayTime)
+}
